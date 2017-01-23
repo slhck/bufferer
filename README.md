0a73f6f63e644a258f54c08b70b389ee3be8fa59
Werner Robitza <werner.robitza@gmail.com>
2017-01-23 17:12:05 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# Bufferer
+
+Inserts fake rebuffering events into video.
+
+Author: Werner Robitza <werner.robitza@gmail.com>
+
+# Usage
+
+    bufferer.py [-hfn] -i <input> -o <output>
+                [-b <buflist>] [-v <vcodec>] [-a <acodec>]
+                [-s <spinner>] [-p <speed>] [-t <trim>]
+                [--verbose]
+    
+    -h --help                     show help message
+    -f --force                    force overwrite output files
+    -n --dry-run                  only print final command, do not run
+    -i --input <input>            input video file
+    -o --output <output>          output video file
+    -b --buflist <buflist>        list of buffering events in format "[[x1,y1], [x2,y2],...]" or
+                                  "[x1,y1], [x2,y2], ..." where x = position of event in seconds, y = duration of event
+    -v --vcodec <vcodec>          video encoder to use (see `ffmpeg -encoders`) [default: ffv1]
+    -a --acodec <acodec>          audio encoder to use (see `ffmpeg -encoders`) [default: pcm_s16le]
+    -s --spinner <spinner>        path to spinner animated file or video [default: spinners/spinner-256-white.png]
+    -p --speed <speed>            speed of the spinner, rounded to integer [default: 2]
+    -t --trim <trim>              trim video to length in seconds or "HH:MM:SS.msec" format
+    --verbose                     show verbose output
+
+# Requirements
+
+- Python 2.7
+- docopt (`pip install docopt`)
+- FFmpeg (download a static build from [their website](http://ffmpeg.org/download.html))
+
+# Caveats
+
+
+# Acknowledgements
+
+- Big Buck Bunny: Blender Foundation
+- Free spinners from http://preloaders.net/en/free
+- Click from http://metronomer.com/
+
+# Helpful info
+
+To generate AV sync samples:
+
+    ffmpeg -f lavfi -i testsrc=duration=20:size=1280x720:rate=24 \
+    -vf "drawtext=timecode='00\:00\:00\:00':fontsize=72:r=24:x=(w-tw)/2:y=h-(2*lh): \
+    fontcolor=white:box=1:boxcolor=black@1.0" \
+    -c:v huffyuv testsrc_24.avi
+    ffmpeg -f lavfi -i testsrc=duration=20:size=1280x720:rate=23.97 \
+    -vf "drawtext=timecode='00\:00\:00\:00':fontsize=72:r=23.97:x=(w-tw)/2:y=h-(2*lh): \
+    fontcolor=white:box=1:boxcolor=black@1.0" \
+    -c:v huffyuv testsrc_2397.avi
+
+    ffmpeg -i testsrc_24.avi -i click.mp3 -c:v copy -c:a pcm_s16le \
+    -map 0:v -map 1:a testsrc_24_c.avi
+    ffmpeg -i testsrc_2397.avi -i click.mp3 -c:v copy -c:a pcm_s16le \
+    -map 0:v -map 1:a testsrc_2397_c.avi
+
+# License
+
+Copyright (c) 2017 Werner Robitza
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ bufferer.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2017 Werner Robitza
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+Bufferer
+
+Inserts fake rebuffering events into video
+
+Usage:
+    bufferer.py [-hfn] -i <input> -o <output>
+                [-b <buflist>] [-v <vcodec>] [-a <acodec>]
+                [-s <spinner>] [-p <speed>] [-t <trim>]
+                [--verbose]
+
+    -h --help                     show help message
+    -f --force                    force overwrite output files
+    -n --dry-run                  only print final command, do not run
+    -i --input <input>            input video file
+    -o --output <output>          output video file
+    -b --buflist <buflist>        list of buffering events in format "[[x1,y1], [x2,y2],...]" or
+                                  "[x1,y1], [x2,y2], ..." where x = position of event in seconds, y = duration of event
+    -v --vcodec <vcodec>          video encoder to use (see `ffmpeg -encoders`) [default: ffv1]
+    -a --acodec <acodec>          audio encoder to use (see `ffmpeg -encoders`) [default: pcm_s16le]
+    -s --spinner <spinner>        path to spinner animated file or video [default: spinners/spinner-256-white.png]
+    -p --speed <speed>            speed of the spinner, rounded to integer [default: 2]
+    -t --trim <trim>              trim video to length in seconds or "HH:MM:SS.msec" format
+    --verbose                     show verbose output
+"""
+
+from docopt import docopt
+import os
+import subprocess
+import json
+import re
+
+class Bufferer:
+
+    def __init__(self, arguments):
+        # assign arguments from commandline
+        self.input_file      = arguments["--input"]
+        self.output_file     = arguments["--output"]
+        self.spinner         = arguments["--spinner"]
+        self.speed           = int(arguments["--speed"])
+        self.trim            = arguments["--trim"]
+        self.force_overwrite = arguments["--force"]
+        self.dry             = arguments["--dry-run"]
+        self.vcodec          = arguments["--vcodec"]
+        self.acodec          = arguments["--acodec"]
+
+        try:
+          self.buflist = json.loads(arguments["--buflist"])
+        except Exception as e:
+            buflist_mod = "[" + arguments["--buflist"] + "]"
+            try:
+                self.buflist = json.loads(buflist_mod)
+            except Exception as e:
+                raise StandardError("Buffering list parameter not properly formatted. Use a list like [[0, 1], [5, 10]]")
+
+        # get info needed for processing
+        self.parse_fps_samplerate()
+
+
+    @staticmethod
+    def run_command(cmd, raw=True, dry=False):
+        """
+        Run a command directly
+        """
+        if dry:
+            print("[cmd] " + str(cmd))
+            return
+
+        if raw:
+            process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        else:
+            process = subprocess.Popen(cmd.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        stdout, stderr = process.communicate()
+
+        if process.returncode == 0:
+            return stdout + stderr
+        else:
+            print("[error] running command: {}".format(cmd))
+            print(str(stderr))
+
+
+    def parse_fps_samplerate(self):
+        """
+        Get FPS as a float from the input file using ffmpeg
+        """
+
+        p = subprocess.Popen(['ffmpeg', '-i', self.input_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+        output = stderr
+        video_regex = re.compile(r'Video: (.*)')
+        audio_regex = re.compile(r'Audio: (.*)')
+
+        if video_regex.search(output):
+            video_line = video_regex.search(output).group(1)
+            fps_pattern = re.compile(r'.*, ([0-9.]+) fps,.*')
+            fps_match = fps_pattern.search(video_line)
+            if fps_match:
+                self.fps = float(fps_match.group(1))
+
+        if audio_regex.search(output):
+            audio_line = audio_regex.search(output).group(1)
+            hz_pattern = re.compile(r'.*, ([0-9]+) Hz,.*')
+            hz_match = hz_pattern.search(audio_line)
+            if hz_match:
+                self.samplerate = float(hz_match.group(1))
+
+        if not self.fps or not self.samplerate:
+            raise StandardError("[error] could not find video stream or detect fps / samplerate")
+
+
+    def set_loop_cmds(self):
+        loop_cmds            = []
+        aloop_cmds           = []
+        enable_cmds          = []
+
+        total_looped         = 0
+        total_enable         = 0
+        total_looped_samples = 0
+
+        for buf_event in self.buflist:
+            buf_pos, buf_len = buf_event
+
+            # offset buf_position by the total number of looped frames
+            buf_pos_frames = int(self.fps * buf_pos) + total_looped
+            buf_len_frames = int(self.fps * buf_len)
+
+            # offset buf_position by the total number of looped samples
+            buf_pos_samples = int(self.samplerate * buf_pos) + total_looped_samples
+            buf_len_samples = int(self.samplerate * buf_len)
+
+            loop_cmd = "loop=loop={buf_len_frames}:size=1:start={buf_pos_frames},\
+            setpts=N/FRAME_RATE/TB".format(**locals())
+            loop_cmds.append(loop_cmd)
+
+            aloop_cmd = "aloop=loop={buf_len_samples}:size=1:start={buf_pos_samples},\
+            asetpts=N/SAMPLE_RATE/TB".format(**locals())
+            aloop_cmds.append(aloop_cmd)
+
+            buf_pos_enable = buf_pos + total_enable
+            buf_len_enable = buf_pos_enable + buf_len
+            enable_cmd = "between(t,{buf_pos_enable},{buf_len_enable})".format(**locals())
+            enable_cmds.append(enable_cmd)
+
+            total_looped = total_looped + buf_len_frames
+            total_enable = total_enable + buf_len
+
+            total_looped_samples = total_looped_samples + buf_len_samples
+
+        self.loop_cmd = (", ").join(loop_cmds)
+        self.enable_cmd = ("+").join(enable_cmds)
+
+    def set_specs(self):
+        # various ffmpeg options
+        if self.force_overwrite:
+            self.overwrite_spec = "-y"
+        else:
+            self.overwrite_spec = "-n"
+
+        if self.trim:
+            self.trim_spec = "-t " + self.trim
+        else:
+            self.trim_spec = ""
+
+    def insert_buf_audiovisual(self):
+        """
+        Insert the buffering events on both audio and video tracks, looping the video
+        frames and audio samples at the corresponding positions.
+        """
+
+        self.set_loop_cmds()
+        self.set_specs()
+
+        cmd = '''
+        ffmpeg {self.overwrite_spec} -i {self.input_file}
+        -filter_complex "[0:v]{self.loop_cmd}[stallvid];
+        [0:a]{self.aloop_cmd},volume=0:enable='{self.enable_cmd}'[outa];
+        [stallvid]avgblur=5:enable='{self.enable_cmd}', eq=brightness=-0.3:enable='{self.enable_cmd}'[stallvidblur]; \
+        movie=filename={self.spinner}:loop=999999, setpts=N/(FRAME_RATE*TB)*{self.speed}[spinner];
+        [stallvidblur][spinner]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2:shortest=1:enable='{self.enable_cmd}'[outv]"
+        -an -shortest -map "[outv]" -map "[outa]" {self.trim_spec} -c:v {self.vcodec} -c:a {self.acodec} {self.output_file}
+        '''.format(**locals()).replace('\n',' ').replace('  ',' ').strip()
+
+        Bufferer.run_command(cmd, dry=self.dry)
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='bufferer v0.1')
+
+    if not os.path.isfile(arguments["--input"]):
+        raise IOError("Input file does not exist")
+
+    b = Bufferer(arguments)
+    b.insert_buf_audiovisual()
+
+    print "Output written to " + b.output_file
